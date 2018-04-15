0130bfae44bad70d52b14a288210265b8987c25a
Elcoosp <elcoosp@gmail.com>
2018-04-15 22:55:36 +0200

    add: index main entry from cli

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-const commander
-cosmiconfig
-write = require('cosmiconfig')
+const program = require('commander'),
+  splitByType = require('./splitByType'),
+  readTemplatesAndWriteComps = require('./readTemplatesAndWriteComps')
+
+const createNewComps = (comp, otherComps) => {
+  const comps = splitByType(comp, ...otherComps)
+  readTemplatesAndWriteComps(comps)
+}
+
+program.command('new <comp> [otherComps...]').action(createNewComps)
+
+program.parse(process.argv)
